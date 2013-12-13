795ac695f4028444f0ce07f2261f71c32f2bad2e
Javier Collado <javier.collado@gmail.com>
2013-12-13 17:45:58 +0100

    Updated to retrieve project metadata from json file
    Metadata is no longer harcoded in the application code, but in a
    separate file that can be updated later as new projects are being added.

--- js/application.js
+++ js/application.js
@@ -1,37 +1,14 @@
 $(document).ready(function() {
-    app = new ApplicationModel();
+  // Get question data from json file in server
+  $.getJSON('data/projects.json', function(projects) {
+    console.log('Projects metadata', projects);
+
+    // Create application to display given projects
+    app = new ApplicationModel(projects);
     ko.applyBindings(app);
+  });
 });
 
-function ApplicationModel() {
-  this.projects = [
-      {
-        name: 'Hot or cold',
-        description: 'Number guessing game',
-        url: '/HotOrCold',
-        img: 'img/HotOrCold.png',
-        tags: ['html', 'css', 'javascript', 'jquery']
-      },
-      {
-        name: 'Shopping list',
-        description: 'Todo list application',
-        url: '/ShoppingList',
-        img: 'img/ShoppingList.png',
-        tags: ['html', 'css', 'javascript', 'jquery']
-      },
-      {
-        name: 'Quiz',
-        description: 'Programming language quiz',
-        url: '/QuizKnockout',
-        img: 'img/Quiz.png',
-        tags: ['html', 'css', 'less', 'javascript', 'jquery', 'knockout']
-      },
-      {
-        name: 'Do it!',
-        description: 'Todo list application using <a href="http://www.rememberthemilk.com">Remember The Milk</a> as a backend',
-        url: '/DoIt',
-        img: 'img/DoIt.png',
-        tags: ['html', 'css', 'less', 'javascript', 'jquery', 'knockout']
-      }
-    ];
+function ApplicationModel(projects) {
+  this.projects = projects;
 }
