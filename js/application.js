810f739110bf23f1beb5bd60d50c99e9c3c0e9f7
Javier Collado <javier.collado@gmail.com>
2013-12-13 17:35:59 +0100

    Updated to use generate project information from metadata

--- js/application.js
+++ js/application.js
@@ -1,3 +1,37 @@
 $(document).ready(function() {
-    console.log("Hello from jquery!");
+    app = new ApplicationModel();
+    ko.applyBindings(app);
 });
+
+function ApplicationModel() {
+  this.projects = [
+      {
+        name: 'Hot or cold',
+        description: 'Number guessing game',
+        url: '/HotOrCold',
+        img: 'img/HotOrCold.png',
+        tags: ['html', 'css', 'javascript', 'jquery']
+      },
+      {
+        name: 'Shopping list',
+        description: 'Todo list application',
+        url: '/ShoppingList',
+        img: 'img/ShoppingList.png',
+        tags: ['html', 'css', 'javascript', 'jquery']
+      },
+      {
+        name: 'Quiz',
+        description: 'Programming language quiz',
+        url: '/QuizKnockout',
+        img: 'img/Quiz.png',
+        tags: ['html', 'css', 'less', 'javascript', 'jquery', 'knockout']
+      },
+      {
+        name: 'Do it!',
+        description: 'Todo list application using <a href="http://www.rememberthemilk.com">Remember The Milk</a> as a backend',
+        url: '/DoIt',
+        img: 'img/DoIt.png',
+        tags: ['html', 'css', 'less', 'javascript', 'jquery', 'knockout']
+      }
+    ];
+}
